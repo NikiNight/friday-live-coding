b2702a84993620fa4ecfad01810623ca8c0309bd
Viktoriya Vorozhun <viktoriya.vorozhun@gmail.com>
2020-03-20 22:32:10 +0300

    Lecture Part 6

---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+const webpack = require('webpack');
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = (env, options) => {
+    const isProduction = options.mode === 'production';
+
+    const config = {
+        mode: isProduction ? 'production' : 'development',
+        devtool: isProduction ? 'none' : 'source-map',
+        watch: !isProduction,
+        entry: ['./src/index.js', './src/sass/style.scss'],
+        output: {
+            path: path.join(__dirname, '/dist'),
+            filename: 'script.js',
+        },
+
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use: {
+                      loader: 'babel-loader',
+                      options: {
+                        presets: ['@babel/preset-env']
+                      }
+                    }
+                }, {
+                    test: /\.scss$/,
+                    use: [
+                        MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader' 
+                    ]
+                }, {
+                    test: /\.(png|svg|jpe?g|gif)$/,
+                    use: [
+                        {
+                            loader: 'file-loader',
+                        }
+                    ]
+                }, {
+                    test: /\.html$/,
+                    loader: 'html-loader',
+                },
+            ]
+        },
+
+        plugins: [
+            new CleanWebpackPlugin(),
+            new HtmlWebpackPlugin({
+                template: 'index.html'
+            }),
+            new MiniCssExtractPlugin({
+                filename: 'style.css'
+            })
+        ]
+    }
+
+    return config;
+}
